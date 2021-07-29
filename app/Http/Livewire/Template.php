d7163772e6aadd923a8d7a3db8133b450e503cdb
alfajri23 <feri.alfajri@gmail.com>
2021-07-29 11:14:38 +0700

    change uniqid to rand()

--- app/Http/Livewire/Template.php
+++ app/Http/Livewire/Template.php
@@ -35,7 +35,7 @@ class Template extends Component
 
         Note::updateOrCreate(['id' => $this->id_note],[
             //menambahkan unik id karena heroku tdk bisa increment id
-            'id' => uniqid(),
+            'id' => rand(),
             'judul' => $this->judul,
             'desc' => $this->desc,
             'deadline' => $this->deadline,
